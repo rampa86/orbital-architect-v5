05cefeca8cf23c3a0a8307f712fe1a75c5e4fcd4
Ruben Calahorrano <rubencalahorrano@tu-cosita.local>
2026-01-26 00:35:46 +0100

    Deploy to ruben folder

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // CRITICAL: This ensures assets load correctly on shared hosting (SiteGround)
+  base: '/ruben/', // CRITICAL: This ensures assets load correctly on shared hosting (SiteGround)
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
